ea1738bab1448f5481b762850fb34c5d5fdf49cb
rian <rian.rafli@gmail.com>
2023-11-24 04:24:28 +0700

    docs : add update,delete course endpoint, add login,register auth endpoint

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const {app,express} = require('./app')
 const {PORT} = process.env
 const cors = require("cors")
 const {otherError,notFoundError} = require('./middlewares/errorHandling.middleware')
+const docsRoute = require('./routes/v1/docs.routes')
 const authRoute = require('./routes/v1/auth.routes')
 const coursesRoute = require('./routes/v1/course.routes')
 const categoriesRoute = require('./routes/v1/category.routes')
@@ -10,10 +11,12 @@ const bodyParser = require('body-parser')
 
 
 
+
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use('/api/v1',docsRoute)
 app.use('/api/v1/auth',authRoute)
 app.use('/api/v1',coursesRoute)
 app.use('/api/v1',categoriesRoute)
