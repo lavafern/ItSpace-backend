7a058e1a7fe21f95c132a6bf66a24966efd4613f
Azzamaulana26 <azzamaulana26@gmail.com>
2023-11-24 19:33:21 +0700

    updatecourse

--- controllers/course.controller.js
+++ controllers/course.controller.js
@@ -90,5 +90,91 @@ module.exports = {
         }
     },
 
+    updateCourse: async (req, res, next) => {
+        try {
+            // TODO: Implement admin authorization 
+            const courseId = req.params.id;
+            let {
+                title, price, level, isPremium, description, courseCategory, mentorEmail
+            } = req.body;
 
-}
+            price = Number(price);
+
+            if (isNaN(price)) throw new Error("Kolom harga harus diisi angka", { cause: 400 });
+            if (!title || !price || !level || !description) throw new Error("Tolong isi kolom yang wajib di isi", { cause: 400 });
+            if (!(Array.isArray(courseCategory)) || !(Array.isArray(mentorEmail))) throw new Error("courseCategory dan mentorEmail harus array", { cause: 400 });
+            if (!(isPremium === false || isPremium === true)) throw new Error("isPremium harus boolean", { cause: 400 });
+            if (!(level === "BEGINNER" || level === "INTERMEDIATE" || level === "ADVANCED")) throw new Error("level tidak valid", { cause: 400 });
+            if (description.length > 1024) throw new Error("description tidak boleh lebih dari 1024 karakter", { cause: 400 });
+            if (title.length > 60) throw new Error("title tidak boleh lebih dari 60 karakter", { cause: 400 });
+
+            // category data
+            const courseCategoryForPrisma = courseCategory.map((c) => {
+                return { name: c };
+            });
+
+            let categoryId = await prisma.category.findMany({
+                where: {
+                    OR: courseCategoryForPrisma,
+                },
+            });
+
+            const validCategory = categoryId.map((i) => {
+                return i.name;
+            });
+            categoryId = categoryId.map((i) => {
+                return { categoryId: i.id };
+            });
+
+            // mentor data
+            const mentorEmailForPrisma = mentorEmail.map((e) => {
+                return { email: e };
+            });
+
+            let mentorId = await prisma.user.findMany({
+                where: {
+                    OR: mentorEmailForPrisma,
+                },
+            });
+            const mentorValidEmail = mentorId.map((i) => {
+                return i.email;
+            });
+
+            mentorId = mentorId.map((i) => {
+                return { authorId: i.id };
+            });
+
+            // update course
+            const updatedCourse = await prisma.course.update({
+                where: {
+                    id: courseId,
+                },
+                data: {
+                    title,
+                    price,
+                    level,
+                    isPremium,
+                    description,
+                    courseCategory: {
+                        set: categoryId,
+                    },
+                    mentor: {
+                        set: mentorId,
+                    },
+                },
+            });
+
+            updatedCourse.mentor = mentorValidEmail;
+            updatedCourse.category = validCategory;
+
+            res.json({
+                success: true,
+                message: "Successfully update course",
+                data: updatedCourse,
+            });
+        } catch (err) {
+            next(err);
+        }
+    },
+};
+
--- routes/v1/course.routes.js
+++ routes/v1/course.routes.js
@@ -1,6 +1,8 @@
 const {createCourse} = require('../../controllers/course.controller')
+const {updateCourse} = require('../../controllers/course.controller')
 const router = require('express').Router()
 
 router.post('/courses',createCourse)
+router.put('/courses',updateCourse)
 
 module.exports = router
