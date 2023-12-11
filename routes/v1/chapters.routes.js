a51357c42028be15b1c3299c39128dc616e2bf30
Azzamaulana26 <azzamaulana26@gmail.com>
2023-12-11 21:54:35 +0700

    delete chapter

--- routes/v1/chapters.routes.js
+++ routes/v1/chapters.routes.js
@@ -1,7 +1,9 @@
-const {createChapter} = require('../../controllers/chapter.controller')
+const {createChapter,updateChapter,deleteChapter} = require('../../controllers/chapter.controller')
 const router = require('express').Router()
 
 router.post('/courses/:courseId/chapters',createChapter)
+router.put('/courses/:courseId/chapters/:id',updateChapter)
+router.delete('/courses/:courseId/chapters/:id',deleteChapter)
 
 
 module.exports = router
