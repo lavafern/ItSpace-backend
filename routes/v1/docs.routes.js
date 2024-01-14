832ee4e6024a6284df472496c9b4b775d5e8e4b3
rian <rian.rafli@gmail.com>
2024-01-14 12:07:35 +0700

    test : swaggerUi add customCssUrl

--- routes/v1/docs.routes.js
+++ routes/v1/docs.routes.js
@@ -6,9 +6,10 @@ const fs = require('fs');
 const YAML = require('yaml');
 const file  = fs.readFileSync(filepath, 'utf8');
 const swaggerDocument = YAML.parse(file);
+const CSS_URL ='https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 
 
 router.use('/api-docs', swaggerUi.serve);
-router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.get('/api-docs', swaggerUi.setup(swaggerDocument,{ customCssUrl: CSS_URL }));
 
 module.exports = router;
