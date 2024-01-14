2a43215ac4ef0c4a784c673ef6c553b14cdf20f4
rian <rian.rafli@gmail.com>
2024-01-14 12:24:46 +0700

    test : swaggerUi options

--- routes/v1/docs.routes.js
+++ routes/v1/docs.routes.js
@@ -6,12 +6,13 @@ const fs = require('fs');
 const YAML = require('yaml');
 const file  = fs.readFileSync(filepath, 'utf8');
 const swaggerDocument = YAML.parse(file);
+const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css';
 
-const options = {
-    customCss: '.swagger-ui .topbar { display: none }'
-};
-
-router.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerDocument, options));
+router.use('/api-docs', swaggerUi.serve,swaggerUi.setup(swaggerDocument,  {
+    customCss:
+      '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}));
 // router.get('/api-docs', swaggerUi.setup(swaggerDocument, options));
 
 module.exports = router;
